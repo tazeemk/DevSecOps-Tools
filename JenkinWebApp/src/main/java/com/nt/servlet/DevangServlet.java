10d9c94ea1b560cb08b726874b85987fed25bccd
Tazeem Khan <tazeemk329@gmail.com>
2024-11-26 14:35:27 +0530

    change1

--- JenkinWebApp/src/main/java/com/nt/servlet/DevangServlet.java
+++ JenkinWebApp/src/main/java/com/nt/servlet/DevangServlet.java
@@ -22,6 +22,7 @@ public class DevangServlet extends HttpServlet
 		//pw.format("text/html");
 		pw.print("Welcome TO DevangPelanduHub :");
 		pw.print(new Date());
+		pw.print("hello ");
 		
 		
 		
