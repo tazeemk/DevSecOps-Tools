998d77cad7186751b08ae593bf25e0d296bcd873
tazeemk <tazeemk329@gmail.com>
2025-03-23 16:33:53 +0530

    Gautam

--- JenkinWebApp/src/main/java/com/nt/servlet/DevangServlet.java
+++ JenkinWebApp/src/main/java/com/nt/servlet/DevangServlet.java
@@ -21,7 +21,7 @@ public class DevangServlet extends HttpServlet
 		//pw.format("text/html");
 		pw.print("Welcome TO DevangPelanduHub :");
 		pw.print(new Date());
-		pw.print("<br>" + " hello ");
+		pw.print("<br>" + " hello Gautam ");
 		
 		pw.print("<a href ='index.jsp'>click here to return home page" );
 		
